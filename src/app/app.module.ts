71cadb8dd668c00db921cc9c50c6503e9cedead8
Julian Slonim <jslonim@overactive.com>
2019-12-02 16:10:02 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { ImageCropperModule } from 'ngx-image-cropper';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
